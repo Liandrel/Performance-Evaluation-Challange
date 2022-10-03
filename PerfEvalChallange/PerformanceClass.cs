617d2914a93ce27e7b4e74eb838d1179bf36e8e9
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-03 20:55:40 +0200

    Performance Count Method
    Added static class with method that calculate time of Action method sent as argument

---
+++ PerfEvalChallange/PerformanceClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfEvalChallange
+{
+    public static class PerformanceClass
+    {
+        public static void CalculateSpeed(Action<int> methodToTest, int[] repetitions, string testName)
+        {
+            for (int i = 0; i < repetitions.Length; i++)
+            {
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                stopwatch.Start();
+
+                methodToTest(repetitions[i]);
+
+                stopwatch.Stop();
+                Console.WriteLine($"{testName} {repetitions[i]} reps: {stopwatch.ElapsedMilliseconds} ms");
+            }
+        }
+    }
+}
