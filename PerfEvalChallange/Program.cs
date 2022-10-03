2e307b2bd3d0ea72e23868e0cc329393419d8bcf
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-03 20:28:48 +0200

    Basic Challange
    Added code for basic Performance Evaluation Challange

--- PerfEvalChallange/Program.cs
+++ PerfEvalChallange/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Text;
+
+//string test
+Stopwatch stopwatch = Stopwatch.StartNew();
+stopwatch.Start();
+
+string test = "";
+for (int i = 0; i < 50000; i++)
+{
+    test += "Testing";
+}
+
+stopwatch.Stop();
+Console.WriteLine($"Time Taken {stopwatch.ElapsedMilliseconds} ms");
+
+
+
+//stringbuilder test
+stopwatch = Stopwatch.StartNew();
+stopwatch.Start();
+
+StringBuilder sb = new();
+for (int i = 0; i < 500000; i++)
+{
+    sb.Append("Testing");
+}
+
+string sbOut = sb.ToString();
+
+stopwatch.Stop();
+Console.WriteLine($"Time Taken {stopwatch.ElapsedMilliseconds} ms");
